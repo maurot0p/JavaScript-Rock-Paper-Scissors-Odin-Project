e362763b46fc8f6c1f22f87d7e2b71f1fcbb1e27
maurot0p <mauricioguzmannova@gmail.com>
2021-10-12 19:54:07 -0400

    update

--- rock.js
+++ rock.js
@@ -2,9 +2,8 @@ function computerPlay(){
     var arr=["Rock","Paper","Scissor"];
     return arr[Math.floor(Math.random()*3)];
 }
-function playRound() {
+function playRound(playerSelection) {
     var computerSelection = computerPlay();
-    var playerSelection = prompt();
     if(playerSelection==computerSelection) {
         return "ITS A TIE";
     }
@@ -14,30 +13,54 @@ function playRound() {
     else if (playerSelection=="Paper" && computerSelection=="Rock"){
         return "Player wins";
     }
+    else if(playerSelection=="Scissor" && computerSelection=="Paper"){
+      return "Player wins";
+    }
     else{
         return "Computer wins";
     }
   }
-  function game(){
-      var count_player=0;
-      var count_computer=0;
-        for(var i=1;i<6;i++){
-            if(playRound()=="Player wins") {
-                console.log("Player wins round" +i);
-                count_player++;
-            }
+  const container=document.querySelector('#container');
+  const result_computer=document.querySelector('#computer');
+  const result_player=document.querySelector('#player');
+  const winner =document.querySelector('#winner');
+  const tie=document.querySelector('#tie');
+  var count_player=0;
+  var count_computer=0;
+  const buttons = document.querySelectorAll('button');
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      if(playRound(button.id)=="Player wins") {
+        tie.style.display="none";
+        count_player++;
+        result_player.textContent="Player:"+count_player;
+    }
+    else if(playRound(button.id)=="Computer wins") {
+      tie.style.display="none";
+      count_computer++;
+      result_computer.textContent="Computer:"+count_computer;
+    }
+    else{
+      tie.style.display="block";
+      tie.textContent="IT WAS A TIE!";
+    }
+    if(count_player>=5){
+      winner.style.display = "block";
+      winner.textContent="Player wins!";
+      count_player=0;
+      count_computer=0;
+      result_computer.textContent="Computer:"+count_computer;
+      result_player.textContent="Player:"+count_player;
+    }
+    if(count_computer>=5){
+      winner.style.display = "block";
+      winner.textContent="Computer wins!";
+      count_player=0;
+      count_computer=0;
+      result_computer.textContent="Computer:"+count_computer;
+      result_player.textContent="Player:"+count_player;
+    }
+    });
+  });
 
-            else if(playRound()=="Computer wins") {
-                console.log("Computer wins round "+i);
-                count_computer++;
-            }
-            else{
-                console.log("Round " +i +" was a tie");
-            }
-        }
-        if(count_player>count_computer)console.log("PLAYER WINS");
-        else if(count_computer>count_player)console.log("COMPUTER WINS");
-        else console.log("ITS A TIE");
-  }
-const playerSelection = "Paper";
-console.log(game());
+  
